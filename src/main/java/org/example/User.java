46f6158343a838ea907a1d51fb1aae4d056de2fb
Lumen <lumenhorizon@icoud.com>
2026-02-16 23:13:41 +0700

    User class

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public record User(String username, String fullName, String email) {
+
+    private static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9_]{3,20}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[^@]+@[^@]+\\.[^@]+$");
+
+
+    public static User create(String username, String fullName, String email) {
+        if (username == null || fullName == null || email == null || username.isBlank() || fullName.isBlank() || email.isBlank()) {
+            throw new IllegalArgumentException("Все поля содержать значения");
+        }
+
+        if (!USERNAME_PATTERN.matcher(username).matches()) {
+            throw new IllegalArgumentException("username должен содержать только латинские буквы, цифры и подчёркивание (3–20 символов)");
+        }
+
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new IllegalArgumentException("email должен соответствовать формату");
+        }
+        return new User(username, fullName, email);
+    }
+
+    public String format() {
+        return "%s (%s) <%s>".formatted(username, fullName, email);
+    }
+}
