6b4ab4ab5022b80207650c74eb277e2af865fd83
Lumen <lumenhorizon@icoud.com>
2026-02-17 12:45:47 +0700

    UserFilter

--- src/main/java/org/example/filter/UserFilter.java
+++ src/main/java/org/example/filter/UserFilter.java
@@ -6,5 +6,13 @@ import org.example.User;
 public interface UserFilter {
 
     boolean test(User user);
+
+    default UserFilter and(UserFilter other) {
+        return user -> test(user) && other.test(user);
+    }
+
+    default UserFilter or(UserFilter other) {
+        return user -> test(user) || other.test(user);
+    }
 }
 
--- src/main/java/org/example/filter/UserFilters.java
+++ src/main/java/org/example/filter/UserFilters.java
@@ -1,4 +1,26 @@
 package org.example.filter;
 
+import java.util.Locale;
+
 public class UserFilters {
+    static UserFilter byUsername(String username) {
+        return user -> user.username().equals(username);
+    }
+
+    static UserFilter byUsernameContains(String substring) {
+        return user -> user.username().toLowerCase().contains(substring.toLowerCase(Locale.ROOT));
+    }
+
+    static UserFilter byEmail(String email) {
+        return user -> user.email().equals(email);
+    }
+
+    static UserFilter byEmailDomain(String domain) {
+        return user -> user.email().matches(".*@%s".formatted(domain));
+    }
+
+    static UserFilter byFullNameContains(String substring) {
+        return user -> user.fullName().toLowerCase(Locale.ROOT).contains(substring.toLowerCase());
+    }
+
 }
