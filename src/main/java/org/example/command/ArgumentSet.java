4a4268925ddad9ede828eedc31464f772d67b408
Lumen1024 <lumenhorizon@icloud.com>
2026-03-02 18:06:40 +0700

    add help functiona

--- src/main/java/org/example/command/ArgumentSet.java
+++ src/main/java/org/example/command/ArgumentSet.java
@@ -2,9 +2,22 @@ package org.example.command;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public record ArgumentSet(
         Map<String, List<String>> flags,
         List<String> base_args
 ) {
+
+    public boolean checkFlag(String flag) {
+        return flags.containsKey(flag);
+    }
+
+    public Optional<String> getFlagValue(String flag) {
+        return Optional.ofNullable(flags.get(flag)).map(List::getFirst);
+    }
+
+    public Optional<List<String>> getFlagValues(String flag) {
+        return Optional.ofNullable(flags.get(flag));
+    }
 }
