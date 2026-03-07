9596372436b1304bbe1934038ef1d97a33db0cf2
Lumen1024 <lumenhorizon@icloud.com>
2026-03-08 00:03:00 +0700

    user commands

--- src/main/java/org/example/command/CommandRegistry.java
+++ src/main/java/org/example/command/CommandRegistry.java
@@ -53,6 +53,101 @@ public class CommandRegistry {
                     }
                 }
         ));
+        parser.registerCommand(new Command(
+                "user-create",
+                """
+                        Создает пользователя
+                        usage: user-create <username> <fullname> <email>""",
+                new HashMap<>(),
+                3,
+                (_, system, args) -> {
+                    var username = args.baseArgs().getFirst();
+                    var fullname = args.baseArgs().get(1);
+                    var email = args.baseArgs().get(2);
+
+                    try {
+                        var user = User.create(username, fullname, email);
+                        system.getUserManager().add(user);
+                    } catch (Exception e) {
+                        System.out.println("Ошибка: " + e.getMessage());
+                    }
+                }
+        ));
+        parser.registerCommand(new Command(
+                "user-view",
+                """
+                        Информация о пользователе
+                        usage: user-view <username>""",
+                (_, system, args) -> {
+                    var username = args.baseArgs().getFirst();
+                    system.getUserManager().findByUsername(username).ifPresentOrElse(
+                            u -> System.out.println(u.format()),
+                            () -> System.out.println("Пользователя не найдено")
+                    );
+                }
+        ));
+        parser.registerCommand(new Command(
+                "user-update",
+                """
+                        Обновляет fullname и/или email пользователя
+                        usage: user-update <flags> <username>
+                        flags:
+                            --fullname <value> - обновление fullname
+                            --email <value> - обновление email""",
+                Map.ofEntries(
+                        Map.entry("--email", 1),
+                        Map.entry("--fullname", 1)
+                ),
+                1,
+                (_, system, args) -> {
+                    var username = args.baseArgs().getFirst();
+                    var fullname = args.getFlagValue("--fullname");
+                    var email = args.getFlagValue("--email");
+
+                    if (email.isEmpty() && fullname.isEmpty()) {
+                        System.out.println("Не передано флагов для обновления полей");
+                        return;
+                    }
+
+                    system.getUserManager().findByUsername(username).ifPresentOrElse(
+                            u -> system.getUserManager().update(
+                                    username,
+                                    fullname.orElseGet(u::fullName),
+                                    email.orElseGet(u::email)
+                            ),
+                            () -> System.out.println("Пользователя не найдено")
+                    );
+                }
+        ));
+        parser.registerCommand(new Command(
+                "user-delete",
+                """
+                        Удаление пользователя и его назначений
+                        usage: user-delete <username>""",
+                new HashMap<>(),
+                1,
+                (scanner, system, args) -> {
+                    var username = args.baseArgs().getFirst();
+                    var user = system.getUserManager().findByUsername(username);
+                    if (user.isEmpty()) {
+                        System.out.println("Пользователя не найдено");
+                        return;
+                    }
+                    System.out.printf("""
+                            Точно хотите удалить пользователя:
+                                %s
+                            [Y/N]:
+                            """, user.get().format());
+
+                    if (scanner.next().equalsIgnoreCase("y")) {
+                        for (var a : system.getAssignmentManager().findByUser(user.get()))
+                            system.getAssignmentManager().remove(a);
+                        system.getUserManager().remove(user.get());
+                    }
+                }
+        ));
+    }
+
 
 
     }
