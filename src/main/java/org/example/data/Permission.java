3b129315fbe5c8841563a3ced822535a51329e49
Lumen1024 <lumenhorizon@icloud.com>
2026-03-09 21:05:12 +0700

    ref validation data classes

--- src/main/java/org/example/data/Permission.java
+++ src/main/java/org/example/data/Permission.java
@@ -1,5 +1,7 @@
 package org.example.data;
 
+import org.example.utils.ValidationUtils;
+
 public record Permission(
         String name,
         String resource,
@@ -7,13 +9,11 @@ public record Permission(
 ) {
 
     public Permission {
-        if (name == null || resource == null || description == null || name.isBlank() || resource.isBlank() || description.isBlank()) {
-            throw new IllegalArgumentException("Все поля должны содержать значения");
-        }
-
-        if (name.contains(" ")) {
+        ValidationUtils.requireNonEmpty(name, "name");
+        ValidationUtils.requireNonEmpty(resource, "resource");
+        ValidationUtils.requireNonEmpty(description, "description");
+        if (name.contains(" "))
             throw new IllegalArgumentException("name не должно содержать пробелов");
-        }
 
         name = name.toUpperCase();
         resource = resource.toLowerCase();
